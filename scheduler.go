792abe07ec30ed0f67597ca0411331b53e0369ee
Davinder Pal <davinder.pal@paysafe.com>
2017-10-31 15:15:12 +0530

    1. chnaged Port Number 2. added sample json file 3. added response code and error

--- scheduler.go
+++ scheduler.go
@@ -5,23 +5,28 @@ import (
 	"os/exec"
 )
 
-func adhoc(w http.ResponseWriter, req *http.Request) {
-	// app := "echo"
-	// arg1 := "Running My first Commandss in Golang"
+type queryJson struct {
+	Bash  string `json:"bash"`
+	Name  string `json:"name"`
+	Query string `json:"query"`
+}
 
-	cmd := exec.Command("echo", "HOla")
+func adhoc(w http.ResponseWriter, req *http.Request) {
+	cmd := exec.Command("sh","sleep 10;echo hola10")
 	stdout, err := cmd.Output()
 
 	if err != nil {
 		println(err.Error())
-		return
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(err.Error()))
 	}
 
-	println(string(stdout))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(string(stdout)))
 }
 
 func main() {
 	router := &http.ServeMux{}
 	router.HandleFunc("/adhoc", adhoc)
-	http.ListenAndServe(":80", router)
+	http.ListenAndServe(":5000", router)
 }
